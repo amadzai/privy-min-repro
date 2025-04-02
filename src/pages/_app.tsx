31de2409bbfc58919c058596c2f9819f1dca8068
Amad Zai <20966134+amadzai@users.noreply.github.com>
2025-04-02 14:46:15 +0800

    build: Install viem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,50 @@
 import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
+import { WagmiProvider } from "@privy-io/wagmi";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { config } from "@/config/wagmi";
+import { PrivyProvider } from "@privy-io/react-auth";
+import { scrollSepolia, scroll } from "viem/chains";
+
+import { SmartWalletsProvider } from "@privy-io/react-auth/smart-wallets";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      config={{
+        // Customize Privy's appearance in your app
+        defaultChain: scrollSepolia,
+        supportedChains: [scrollSepolia, scroll],
+        appearance: {
+          theme: "dark",
+          accentColor: "#676FFF",
+          logo: "/foresightLogo.jpg",
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: "all-users",
+          showWalletUIs: true,
+        },
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <WagmiProvider config={config}>
+          <SmartWalletsProvider>
+            <Component {...pageProps} />
+          </SmartWalletsProvider>
+        </WagmiProvider>
+      </QueryClientProvider>
+    </PrivyProvider>
+  );
 }
